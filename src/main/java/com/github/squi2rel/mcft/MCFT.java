0aef18d8b94ffb53caa5df08addd8742c39f9c05
squi2rel <501410836@qq.com>
2025-06-12 23:46:42 +0800

    fix

--- src/main/java/com/github/squi2rel/mcft/MCFT.java
+++ src/main/java/com/github/squi2rel/mcft/MCFT.java
@@ -85,7 +85,7 @@ public final class MCFT extends JavaPlugin implements CommandExecutor, PluginMes
                 }
                 case "mcft:tracking_update" -> {
                     FTModel model = models.get(player.getUniqueId());
-                    if (model == null || System.currentTimeMillis() - model.lastReceived - 10 < 1000 / fps) return;
+                    if (model == null || System.currentTimeMillis() - model.lastReceived + 10 < 1000 / fps) return;
                     byte[] ref = new byte[buf.readShort()];
                     buf.readBytes(ref);
                     model.readSync(ref);
